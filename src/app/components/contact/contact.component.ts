909fcf1e1dae6cab1682db96e26a69176a391046
ktroude <troude.kev@gmail.com>
2024-10-16 16:41:03 +0200

    add contact

--- src/app/components/contact/contact.component.ts
+++ src/app/components/contact/contact.component.ts
@@ -5,8 +5,20 @@ import { Component } from '@angular/core';
   standalone: true,
   imports: [],
   templateUrl: './contact.component.html',
-  styleUrl: './contact.component.css'
+  styleUrls: ['./contact.component.css'] // Assurez-vous que l'URL est correcte
 })
 export class ContactComponent {
+  
+  // Function to trigger the mailto link
+  onContactClick() {
+    const email = 'troude.kev@gmail.com';
+    const subject = encodeURIComponent("Let's work together"); // Encode le sujet pour le format URL
+    const body = encodeURIComponent('Hi Kevin,'); // Encode le corps pour le format URL
 
+    // Create the mailto URL
+    const mailtoLink = `mailto:${email}?subject=${subject}&body=${body}`;
+
+    // Trigger the mailto link
+    window.location.href = mailtoLink; // Ouvre l'éditeur d'email par défaut
+  }
 }
